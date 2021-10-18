79675c4840da9555c2cb43d281435410d8dffca4
Dragomir Mihai <mischudrag@gmail.com>
2021-10-19 01:00:38 +0300

    Added

---
+++ README.md
@@ -0,0 +1,18 @@
+## <div style='color: lightblue' align=center>Ever wanted a cool or short username on GitHub?</div>
+
+Well, you are in the right place. You can use this little script I made to generate and check if some usernames are available.
+
+
+## <div style='color: lightblue' align=center>How to run?</div>
+
+Python is required. Download it from [here](https://python.org).
+```
+python main.py
+```
+Then just type the options you want to choose.
+
+## <div style='color: lightblue' align=center>Coming soon!</div>
+```
+1. Saving to an .txt file
+2. Coloring? *maybe*
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import random, string, requests, os
+
+os.system('cls' or 'clear')
+
+letters = int(input('How many letters: '))
+num = int(input('How many usernames: '))
+
+print("""
+[1] Digit
+[2] Letter
+[3] Combined
+""")
+
+option = int(input('Choose: '))
+def checkOption(option):
+    if option is 1: username = ('').join(random.choices(string.digits, k=letters))
+    if option is 2: username = ('').join(random.choices(string.ascii_lowercase, k=letters))
+    if option is 3: username = ('').join(random.choices(string.ascii_lowercase + string.digits, k=letters))
+    return username
+
+working = 0
+def run():
+    try:
+        username = checkOption(option)
+        r = requests.get('https://github.com/@' + username)
+    except Exception as error:
+        print(error)
+    if r.status_code == 200:
+        print('[-] ' + username)
+    else:
+        global working
+        working+=1
+        print('[+] ' + username)
+
+for i in range(int(num)):
+    run()
