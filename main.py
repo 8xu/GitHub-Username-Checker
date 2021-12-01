33e117177ba3975dae86b587e276c72d7f841955
Dragomir Mihai <mischudrag@gmail.com>
2021-12-01 19:07:36 +0200

    Update main.py
    Did small things just to not let this repo die.

--- main.py
+++ main.py
@@ -5,24 +5,20 @@ os.system('cls' or 'clear')
 letters = int(input('How many letters: '))
 num = int(input('How many usernames: '))
 
-print("""
-[1] Digit
-[2] Letter
-[3] Combined
-""")
+print("""[1] Digit [2] Letter [3] Combined""")
 
 option = int(input('Choose: '))
 def checkOption(option):
-    if option is 1: username = ('').join(random.choices(string.digits, k=letters))
-    if option is 2: username = ('').join(random.choices(string.ascii_lowercase, k=letters))
-    if option is 3: username = ('').join(random.choices(string.ascii_lowercase + string.digits, k=letters))
+    if option == 1: username = ('').join(random.choices(string.digits, k=letters))
+    if option == 2: username = ('').join(random.choices(string.ascii_lowercase, k=letters))
+    if option == 3: username = ('').join(random.choices(string.ascii_lowercase + string.digits, k=letters))
     return username
 
 working = 0
 def run():
     try:
         username = checkOption(option)
-        r = requests.get('https://github.com/@' + username)
+        r = requests.get('https://api.github.com/users/' + username)
     except Exception as error:
         print(error)
     if r.status_code == 200:
